d5ca9028bc08e44ab6bba8490248a65b22d48bc7
taylormerrill <taylormerrill92@gmail.com>
2018-11-13 21:18:28 -0700

    finished the extended display option

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -10,31 +10,29 @@ const bookmarkList = (function () {
             render();
         })
     };
+
     const detailedViewClick = function () {
         $('.bookmark-list').on('click','.shopping-item',e => {
-            // console.log('button clicked'); 
             e.preventDefault();
             store.toggleExpandedFilter();
-            console.log(e.currentTarget); 
-            const itemIndex =store.items.findIndex(element => element); 
-            const currentObject = store.items[itemIndex];
-            console.log(currentObject);
-            console.log(currentObject.id); 
-
-            console.log(itemIndex); 
+            const currentTargetId = getItemIdFromElement(e.currentTarget); 
+            const currentObject = store.findById(currentTargetId);
+            currentObject.expanded = store.detailedViewVisible; 
+            const objectIndex = store.items.findIndex(element => element.id === currentTargetId);
+            const findAndDisplayExtended = function() {        
+                if (currentObject.expanded) {    
+                        store.items.splice(objectIndex,1, currentObject);
+                };
+            };
+            findAndDisplayExtended();  
             render();
-            
-            // store.idFromElement = id; 
-            // console.log(store.detailedViewVisible);
-            // console.log(id);
-            // console.log(getItemIdFromElement(e.currentTarget));
-            // console.log(store.idFromElement); 
-            render();
-        })
+            console.log('detailed form should be visible');  
+        });
+        
+    }
 
-    };
-    const ratingSelect = function(){
-        $('.submit-rating').click( function(){
+    const ratingSelect = function() {
+        $('.submit-rating').click( function() {
             console.log('submit rating drop-down has been clicked!');
             $(this).children("ul").css('display:none').toggle();
         });
@@ -44,166 +42,147 @@ const bookmarkList = (function () {
         $('.div-form').on('submit','#js-bookmark-list-form', function (event) {
           console.log('save button has been submitted!');
           event.preventDefault();
-        //   const newItemName = $('.js-shopping-list-entry').val();
+          store.toggleAddFormVisible(); 
           const newBookmark = $(event.target).serializeJson(); 
           console.log(newBookmark); 
           api.createItems(newBookmark, (newItem) =>{
-            store.addItem(newItem); 
-            render(); 
-          });
-        }
-    )};
+          store.addItem(newItem);
+          render();
+         });
+        })   
+    };
 
     $.fn.extend({
         serializeJson: function() {
-          const formData = new FormData(this[0]);
-          const o = {};
-          formData.forEach((val, name) => o[name] = val);
-          return JSON.stringify(o);
+        const formData = new FormData(this[0]);
+        const o = {};
+        formData.forEach((val, name) => o[name] = val);
+        return JSON.stringify(o);
         }
-      });
-      function getItemIdFromElement(item) {
-        console.log($(item).closest('.js-item-element').data('data-item-id'));
-        // return $(item)
-          
-      }
+    });
+
+    function getItemIdFromElement(item) {
+        return $(item).closest('.js-item-element').data('item-id');
+    }
+
     function generateItemElement(item) {
         console.log(item); 
+        const width = 16 * $('.rating-input').val();
         const expandedClass = item.expanded ? 'bookmark-item-expanded' : '';
-       let itemTitle = `<a href="#"class="shopping-item ${expandedClass}">${item.title}</a>`;
-       
-       return `
-        <li class="js-item-element" data-item-id=${item.id}>
-       ${itemTitle}
-       <div class="ratingContainer">
-       <div>
-       <img src="https://image.ibb.co/jpMUXa/stars_blank.png" alt="img">
-       </div>
-       <div class="cornerimage" style="width:75%;">
-       <img src="https://image.ibb.co/caxgdF/stars_full.png" alt="">
-       </div>
-    </div>
-        <div class="shopping-item-controls">
-            <button class="shopping-item-delete js-item-delete">
+        let itemTitle = `<a href="#"class="shopping-item ${expandedClass}">${item.title}</a>`;
+        console.log(!store.detailedViewVisible);
+        if(!item.expanded){
+            console.log('detailed form should not be visible');
+            return `
+            <li class="js-item-element" data-item-id=${item.id}>
+            ${itemTitle}
+            <div class="ratingContainer">
+                <div>
+                    <img src="https://image.ibb.co/jpMUXa/stars_blank.png" alt="img">
+                    </div>
+                    <div class="cornerimage" style="width:${width}px;">
+                    <img src="https://image.ibb.co/caxgdF/stars_full.png" alt="">
+                </div>
+            </div>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-delete js-item-delete">
                 <span class="button-label">Delete</span>
-            </button>
-        </div>
-    </li>`;
-      }
+                </button>
+            </div>
+            </li>`;
+        }
 
-function generateShoppingItemsString(itemA) {
-    console.log(itemA);
-    const items = itemA.map((item) => generateItemElement(item));
-    return items;
-  }
-  
-  
-  function render() {
-    let items = [...store.items];
-    newPerspective(items); 
+        else {
+            console.log('detailed for should be visible'); 
+            return `
+                <li class="js-item-element" data-item-id="${item.id}">
+                    ${itemTitle}
+                    <div class="shopping-item-controls">
+                    <button class="shopping-item-edit js-item-edit">
+                    <span class="button-label">Edit</span>
+                    </button>
+                    <button class="detailed-item-toggle js-detailed-item-toggle">
+                    <span class="button-label">link</span>
+                    </button>
+                    </div>
+                </li>`
+                ;
+        }
+    }
 
-    console.log(items); 
-    const shoppingListItemsString = generateShoppingItemsString(items);
-
+    function generateShoppingItemsString(itemA) {
+        console.log(itemA);
+        const items = itemA.map((item) => generateItemElement(item));
+        return items;
+    }
   
-    // insert that HTML into the DOM
-    $('.bookmark-list').html(shoppingListItemsString);
-    console.log('`render` ran');
-  }
+    function render() {
+        let items = [...store.items];
+        console.log(store.items);
+        newPerspective(); 
+        // findAndDisplayExtended(); 
+        console.log(items); 
+        const shoppingListItemsString = generateShoppingItemsString(items);
+        // insert that HTML into the DOM
+        console.log(shoppingListItemsString);
+        $('.bookmark-list').html(shoppingListItemsString);
+        console.log('`render` ran');
+    }
   
-    const newPerspective = function(object) {
-        // console.log('perspective called');
-        console.log(getItemIdFromElement(object)); 
-        const idFromElement = getItemIdFromElement(object);
-        const editCurrentItemObject = store.findById(idFromElement);
-        console.log(editCurrentItemObject);
-     if (store.addFormVisible) {
+    const newPerspective = function() {
+        if(store.addFormVisible) {
             console.log('form should be visible');
             $('#js-bookmark-list-form').html(
-                `<input type='text' class='input-title js-title-input' name = 'title' placeholder='Add Title Here'>
-      <input type='text' name = 'url' class='input-url js-url-input' placeholder='Add URL Here'>
-      <input name= 'rating' type='text' class= 'rating-input' placeholder='input your rating here'> 
-      <li class="js-input-element">
-          <input type='text' class='input-description js-description-input' placeholder='Add Description Here'>
-          <div class="save-item-controls">
-      
-              <button class="shopping-item-toggle js-item-toggle" id="save-button" type ='submit'>
-                  <span class="button-label">Save</span>
-              </button>
-          </div>
-      </li>
-      
-      </li>
-     `
-            )
-        }
-     
-        
-     if(!store.addFormVisible ) {
+               `<input type='text' class='input-title js-title-input' name = 'title' placeholder='Add Title Here'>
+                <input type='text' name = 'url' class='input-url js-url-input' placeholder='Add URL Here'>
+                <input name= 'rating' type='text' class= 'rating-input' placeholder='input your rating here'> 
+                <li class="js-input-element">
+                <input type='text' class='input-description js-description-input' placeholder='Add Description Here'>
+                <div class="save-item-controls">
+                <button class="shopping-item-toggle js-item-toggle" id="save-button" type ='submit'>
+                <span class="button-label">Save</span>
+                </button>
+                </div>
+                </li>
+                </li>`  
+             )
+        } else {
             console.log('form is invisible');
             $('#js-bookmark-list-form').html('');
         }
-           
-    // if(!store.detailedViewVisible){
-    //         console.log('detailedForm is invisible');
-    //         (currentItemObject.id).html('');
-    //     }
+    }
 
-}
-const findAndDisplayExtended = function(){
-    if (store.detailedViewVisible) {
-        console.log('detailed form should be visible');
-        $('.top-level-class').html(`
-                 <li class="js-item-element" data-item-id="${currentItemObject.id}">
-                  ${currentItemObject.title}
-                  <div class="shopping-item-controls">
-                    <button class="shopping-item-edit js-item-edit">
-                      <span class="button-label">Edit</span>
-                    </button>
-                    <button class="detailed-item-toggle js-detailed-item-toggle">
-                      <span class="button-label">link</span>
-                    </button>
-                  </div>
-                </li>
-                `)
-}
-}
+    function handleDeleteItemClicked() {
+        $('.bookmark-list').on('click', '.js-item-delete', event => {
+            console.log('deleted bitch'); 
+            const id = getItemIdFromElement(event.currentTarget);
 
-function handleDeleteItemClicked() {
-    // like in `handleItemCheckClicked`, we use event delegation
-    $('.bookmark-list').on('click', '.js-item-delete', event => {
-        console.log('deleted bitch'); 
-      // get the index of the item in store.items
-      const id = getItemIdFromElement(event.currentTarget);
-      // delete the item
-      api.deleteItem(id, () =>{
-        store.findAndDelete(id);
-        render();
-      });
-    });
-  }
+            api.deleteItem(id, () =>{
+            store.findAndDelete(id);
+            render();
+            });
+        });
+    }
 
-
-function bindEventListeners() {
-    // addBookmark();
-    bookmarkClick();
-    detailedViewClick();
-    ratingSelect(); 
-    findAndDisplayExtended();
-    // handleSave();  
-    handleNewItemSubmit();
-    // handleItemCheckClicked();
-    handleDeleteItemClicked();
-    // handleEditShoppingItemSubmit();
-    // handleToggleFilterClick();
-    // handleShoppingListSearch();
-    // handleItemStartEditing();
-};
+    function bindEventListeners() {
+        // addBookmark();
+        bookmarkClick();
+        detailedViewClick();
+        ratingSelect(); 
+        // handleSave();  
+        handleNewItemSubmit();
+        // handleItemCheckClicked();
+        handleDeleteItemClicked();
+        // handleEditShoppingItemSubmit();
+        // handleToggleFilterClick();
+        // handleShoppingListSearch();
+        // handleItemStartEditing();
+    };
 
 // This object contains the only exposed methods from this module:
-return {
+    return {
     render: render,
     bindEventListeners: bindEventListeners,
     
-};
-    }());
+    };
+}());
--- scripts/store.js
+++ scripts/store.js
@@ -1,52 +1,48 @@
 
 const store = (function(){
     const items =[]; 
-    const addFormVisible = false; 
-    const detailedViewVisible = false; 
-    const idFromElement = ''; 
+    let addFormVisible = false; 
+    let detailedViewVisible = false; 
+    const idFromElement = '';
+
     const toggleAddFormVisible = function(){
             this.addFormVisible = !this.addFormVisible; 
-    }
-    const toggleViewFormVisible = function (){
-      console.log(detailedViewVisible);
-      this.addFormVisible = !this.addFormVisible; 
-    }
+    };
+
+    const toggleExpandedFilter = function() {
+      this.detailedViewVisible = !this.detailedViewVisible;
+    };
+
     const addItem = function(item) {
       this.items.push(item); 
-    }
+    };
     const findById = function(id) {
         return this.items.find(item => item.id === id);
-      };
-      const findAndDelete = function(id) {
+    };
+    const findAndDelete = function(id) {
     
         const itemIndex = this.items.findIndex(element => element.id === id);
         items.splice(itemIndex,1);
-      };
-      const expandedDisplay = function(){
-            const itemIndex =store.items.findIndex(element => element.id);
-            const currentObject = store.items[itemIndex];
-            return currentObject; 
-      }
-      const findAndUpdate = function(itemId) { 
+    };
+
+    const findAndUpdate = function(itemId) { 
         const itemIndex = this.items.findIndex(element => element.id === itemId); 
          items[itemIndex].expanded = Object.assign(items[itemIndex] ,newData);
       };
-      const toggleExpandedFilter = function() {
-        this.detailedViewVisible = !this.detailedViewVisible;
-      };
+      
     
-      const setItemIsEditing = function(id, isEditing) {
+    const setItemIsEditing = function(id, isEditing) {
         const item = this.findById(id);
         item.isEditing = isEditing;
       };
     
-      const setSearchRating = function(term) {
+    const setSearchRating = function(term) {
         this.searchTerm = term;
       };
     return {
         items, addItem, findById,findAndDelete, findAndUpdate, 
         toggleExpandedFilter, setItemIsEditing, setSearchRating, 
         addFormVisible, toggleAddFormVisible,detailedViewVisible,
-         toggleViewFormVisible, idFromElement, expandedDisplay
-    }
+        idFromElement, 
+    };
 }());
